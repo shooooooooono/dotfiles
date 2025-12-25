850ab97808a9c4182492f33f26663bfadbe3c5b8
shooooooooono <60308633+shooooooooono@users.noreply.github.com>
2025-12-25 10:59:03 +0900

    feature: opacity

--- dot_config/nvim/lua/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/plugins/ui/colorscheme.lua
@@ -13,7 +13,7 @@ return {
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = false,
+        transparent = true,
         dimInactive = false,
         terminalColors = true,
         theme = "wave",
